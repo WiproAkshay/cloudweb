4ac7cc067398b8c9eee11ea2fc110065bc9062ac
DevAK <52381265+WiproAkshay@users.noreply.github.com>
2024-07-20 18:49:41 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, My name is Akshay, batch 35 student!"); 
+        System.out.println("Hello, My name is Akshay,batch35 student and this is 1st programme!"); 
     }
 }
